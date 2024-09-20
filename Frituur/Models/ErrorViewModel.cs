0374362cbc5fc0825179e28352ff39d2bba978ce
MerlijnW3 <merlijnwaaijer03@gmail.com>
2024-09-20 10:07:24 +0200

    Add project files.

---
+++ Frituur/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frituur.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
