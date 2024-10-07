96db6ff216fa3666a3d47ccf1074777cc5e90c03
MerlijnW3 <merlijnwaaijer03@gmail.com>
2024-10-07 10:56:21 +0200

    Swagger API werkt

---
+++ WebAPIOud/Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Frituur.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Frituur.Models.User> User { get; set; } = default!;
+    }
+}
